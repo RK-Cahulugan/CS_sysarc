829e62a78df0f6239fcbc1c91f62029d2c0b90b1
RK <ranziekirth@gmail.com>
2025-08-14 21:07:47 +0800

    Java
    This is make the log in page functionable

---
+++ function.js
@@ -0,0 +1,11 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
+function showRegister() {
+      document.getElementById("login-form").style.display = "none";
+      document.getElementById("register-form").style.display = "block";
+    }
+
+    function showLogin() {
+      document.getElementById("register-form").style.display = "none";
+      document.getElementById("login-form").style.display = "block";
+    }
